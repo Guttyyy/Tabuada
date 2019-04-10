6924827dac5a551735f6421e7f9c664692ea306e
Ricardo Pinheiro <rica.bp1@gmail.com>
2019-04-09 21:31:23 -0300

    Initial commitaqweqwe

---
+++ app/src/main/java/br/org/catolicasc/tabuada/MainActivity.java
@@ -0,0 +1,55 @@
+package br.org.catolicasc.tabuada;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final Object Toast = "";
+    private EditText etNumero;
+    private ListView lvTabuada;
+    private Button btnMostrar;
+
+
+    //Criar list_item na pasta do layout, com valor para root de TextView
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etNumero = findViewById(R.id.etNumero);
+        lvTabuada = findViewById(R.id.lvTabuada);
+        btnMostrar = findViewById(R.id.btnMostrar);
+
+
+         btnMostrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int numero = Integer.valueOf(etNumero.getText().toString());   //Pega o numero e converte para inteiro
+
+
+                List<Integer> tabuada = new ArrayList<>(); //Cria a tabuada //Array preenchido com a tabuada
+                for (int i = 1; i <= 10; i++) {
+                    tabuada.add(i * numero);  //valores que vão ser mostrados
+                }
+
+                ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<>( //Vai atualizar o listView toda vez que o array atualizar
+                        MainActivity.this, R.layout.list_item, tabuada // São passadas 3 normas
+                );
+                lvTabuada.setAdapter(arrayAdapter);   //Coloca na listView, vai mostrar os valores na lista
+            }
+        });
+
+    }
+}
+
