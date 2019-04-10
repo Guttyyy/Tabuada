382dc4b975acfb49661a845bf0a60a7aa01f59c2
Ricardo Pinheiro <rica.bp1@gmail.com>
2019-04-09 21:51:08 -0300

    aAÇsdasdasdtiopṕoiuyuioyrtyui

--- app/src/main/java/br/org/catolicasc/tabuada/MainActivity.java
+++ app/src/main/java/br/org/catolicasc/tabuada/MainActivity.java
@@ -1,12 +1,13 @@
 package br.org.catolicasc.tabuada;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,18 +36,26 @@ public class MainActivity extends AppCompatActivity {
          btnMostrar.setOnClickListener(new View.OnClickListener() {   //Cria o listener para o botão
             @Override
             public void onClick(View v) {
-                int numero = Integer.valueOf(etNumero.getText().toString());   //Pega o numero e converte para inteiro
+                
+                try{ //Vai tentar ler o número 
+                    int numero = Integer.valueOf(etNumero.getText().toString());   //Pega o numero e converte para inteiro
 
 
-                List<Integer> tabuada = new ArrayList<>(); //Cria a tabuada //Array preenchido com a tabuada
-                for (int i = 1; i <= 10; i++) {
-                    tabuada.add(i * numero);  //valores que vão ser mostrados
+                    List<Integer> tabuada = new ArrayList<>(); //Cria a tabuada //Array preenchido com a tabuada
+                    for (int i = 1; i <= 10; i++) {
+                        tabuada.add(i * numero);  //valores que vão ser mostrados
+                    }
+
+                    ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<>( //Vai atualizar o listView toda vez que o array atualizar
+                            MainActivity.this, R.layout.list_item, tabuada // São passadas 3 normas
+                    );
+                    lvTabuada.setAdapter(arrayAdapter);   //Coloca na listView, vai mostrar os valores na lista
+                    
+                }catch (NumberFormatException e){   //se digitar errado vem isso
+
                 }
 
-                ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<>( //Vai atualizar o listView toda vez que o array atualizar
-                        MainActivity.this, R.layout.list_item, tabuada // São passadas 3 normas
-                );
-                lvTabuada.setAdapter(arrayAdapter);   //Coloca na listView, vai mostrar os valores na lista
+                
             }
         });
 
