6b65dea02cb32bfacd610ea26defafcc86ff7e37
Guttyyy <39389918+Guttyyy@users.noreply.github.com>
2019-04-09 21:57:30 -0300

    Update MainActivity.java

--- app/src/main/java/br/org/catolicasc/tabuada/MainActivity.java
+++ app/src/main/java/br/org/catolicasc/tabuada/MainActivity.java
@@ -1,12 +1,14 @@
 package br.org.catolicasc.tabuada;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,26 +29,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etNumero = findViewById(R.id.etNumero);
-        lvTabuada = findViewById(R.id.lvTabuada);
-        btnMostrar = findViewById(R.id.btnMostrar);
+        etNumero = findViewById(R.id.etNumero);       //Declara a EditText pelo id dele
+        lvTabuada = findViewById(R.id.lvTabuada);     //Declara o ListView pelo id
+        btnMostrar = findViewById(R.id.btnMostrar);   //Declara o botão
 
-
-         btnMostrar.setOnClickListener(new View.OnClickListener() {
+        //Quando clica no botão ira fazer as operações abaixo
+         btnMostrar.setOnClickListener(new View.OnClickListener() {   //Cria o listener para o botão
             @Override
             public void onClick(View v) {
-                int numero = Integer.valueOf(etNumero.getText().toString());   //Pega o numero e converte para inteiro
+                
+                try{ //Vai tentar ler o número 
+                    int numero = Integer.valueOf(etNumero.getText().toString());   //Pega o numero e converte para inteiro
 
 
-                List<Integer> tabuada = new ArrayList<>(); //Cria a tabuada //Array preenchido com a tabuada
-                for (int i = 1; i <= 10; i++) {
-                    tabuada.add(i * numero);  //valores que vão ser mostrados
+                    List<Integer> tabuada = new ArrayList<>(); //Cria a tabuada //Array preenchido com a tabuada
+                    for (int i = 1; i <= 10; i++) {
+                        tabuada.add(i * numero);  //valores que vão ser mostrados
+                    }
+
+                    ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<>( //Vai atualizar o listView toda vez que o array atualizar
+                            MainActivity.this, R.layout.list_item, tabuada // São passadas 3 normas
+                    );
+                    lvTabuada.setAdapter(arrayAdapter);   //Coloca na listView, vai mostrar os valores na lista
+                    
+                }catch (NumberFormatException e){   //se digitar errado vem isso
+                    android.widget.Toast.makeText(MainActivity.this, "Digite um número", android.widget.Toast.LENGTH_SHORT).show();
                 }
 
-                ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<>( //Vai atualizar o listView toda vez que o array atualizar
-                        MainActivity.this, R.layout.list_item, tabuada // São passadas 3 normas
-                );
-                lvTabuada.setAdapter(arrayAdapter);   //Coloca na listView, vai mostrar os valores na lista
+                
             }
         });
 
